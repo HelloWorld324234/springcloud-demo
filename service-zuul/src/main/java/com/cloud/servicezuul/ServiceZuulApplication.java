de8dfb56a879f1ce20b93438804dfb2726839d6b
yule <Yl3628691*>
2018-08-07 16:32:06 +0800

    2018/8/7 配置路由网关(zuul)

---
+++ service-zuul/src/main/java/com/cloud/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,23 @@
+package com.cloud.servicezuul;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Auther: yule
+ * @Date: 2018/8/7 0007 16:11
+ * @Description:
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
