fe8c62692bd52f5925e35eda6170adf3879e2055
yule <Yl3628691*>
2018-08-21 16:42:49 +0800

    config-client

---
+++ config-client/src/main/java/com/blog/configclient/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.blog.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: yule
+ * @Date: 2018/8/21 0021 16:37
+ * @Description:
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
+}
