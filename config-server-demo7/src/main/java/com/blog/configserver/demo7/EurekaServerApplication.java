63d67651aaa0d3f089b54fd4093579d7772f1c6a
yule <Yl3628691*>
2018-08-22 09:55:52 +0800

    demo7

---
+++ config-server-demo7/src/main/java/com/blog/configserver/demo7/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.blog.configserver.demo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: yule
+ * @Date: 2018/8/22 0022 09:26
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
